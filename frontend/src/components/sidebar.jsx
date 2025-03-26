7d507ca0ddcc003da40afef947fe7c51648f2783
Ply <hoangphuongly00@gmail.com>
2025-03-26 13:57:39 +0700

    generate

---
+++ frontend/src/components/sidebar.jsx
@@ -0,0 +1,17 @@
+function SideBar () {
+    return (
+        <div className="bg-blue-500 h-screen w-fit flex-col justify-between">
+            <div>Brick & Beam</div>
+            <hr/>
+            <ul>
+                <li>Sản phẩm</li>
+                
+                <li>Giỏ hàng</li>
+                <li>Đơn hàng</li>
+            </ul>
+            <hr/>
+            <div>Account</div>
+        </div>
+    )
+}
+export default SideBar
---
+++ frontend/src/pages/sanpham.jsx
@@ -0,0 +1,6 @@
+function SanPham () {
+    return (
+        <div className="bg-green-200 w-fill">This is SanPham</div>
+    )
+}
+export default SanPham
